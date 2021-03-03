4576584b976ad67a56dad44089e68447d22486a2
mim4 <milundain98@gmail.com>
2021-03-03 15:27:26 +0100

    Using reactive values

--- app.R
+++ app.R
@@ -2,25 +2,37 @@ library(shiny)
 library(gapminder)
 library(tidyverse)
 
+gapminder_years = gapminder %>% select(year) %>% unique %>% arrange
+
 dataPanel <- tabPanel("Data",
                       selectInput(
                         inputId = "selYear",
                         label = "Select the Year",
                         multiple = TRUE,
-                        choices = gapminder %>% select(year) %>% unique %>% arrange,
-                        selected = gapminder %>% select(year) %>% head(1)
+                        choices = gapminder_years,
+                        selected = gapminder_years %>% head(1),
                       ),
                       tableOutput("data")
 )
-
+plotPanel <- tabPanel("Plot",
+                      plotOutput("plot")
+)
 # Define UI for application that draws a histogram
-ui <- fluidPage(title="Shiny App",
-                dataPanel
+ui <- navbarPage("shiny App",
+                 dataPanel,
+                 plotPanel
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  output$data <- renderTable(gapminder %>% filter(year %in% input$selYear))
+server <- function(input, output) { 
+  gapminder_year <- reactive({gapminder %>% filter(year %in% input$selYear)})
+  output$data <- renderTable(gapminder_year());
+  output$plot <- renderPlot(
+    barplot(head(gapminder_year() %>% pull(pop)),
+            main=paste("Population in",input$selYear),
+            names.arg= head(gapminder_year() %>% pull(country))
+    )
+  )
 }
 
 # Run the application 
